fe839b573d0f7fd16035fbcf90c7a21797a6b924
Kalil Holanda <kalilholanda@me.com>
2021-06-19 16:56:08 -0300

    Remove custom Equatable implementation

--- QuizEngineTests/QuestionTest.swift
+++ QuizEngineTests/QuestionTest.swift
@@ -24,16 +24,4 @@ class QuestionTest: XCTestCase {
         
         XCTAssertEqual(sut.hashValue, type.hashValue)
     }
-    
-    func test_equal_isEqual() {
-        XCTAssertEqual(Question.singleAnswer(type), Question.singleAnswer(type))
-        XCTAssertEqual(Question.multipleAnswer(type), Question.multipleAnswer(type))
-    }
-    
-    func test_notEqual_isNotEqual() {
-        XCTAssertNotEqual(Question.singleAnswer(type), Question.singleAnswer(anotherType))
-        XCTAssertNotEqual(Question.multipleAnswer(type), Question.multipleAnswer(anotherType))
-        XCTAssertNotEqual(Question.singleAnswer(type), Question.multipleAnswer(anotherType))
-        XCTAssertNotEqual(Question.singleAnswer(type), Question.multipleAnswer(type))
-    }
 }
